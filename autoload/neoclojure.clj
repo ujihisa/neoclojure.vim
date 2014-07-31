d914c3e103db149d857e7f1a1e296b0a4a67791a
ujihisa <ujihisa@gmail.com>
2014-07-30 23:27:50 -0700

    indentation

--- autoload/neoclojure.clj
+++ autoload/neoclojure.clj
@@ -1,83 +1,83 @@
-  (ns neoclojure
-    (:require [clojure.string :as s]))
+(ns neoclojure
+  (:require [clojure.string :as s]))
 
-  (defn split-at-last-dot [st]
-    (let [[left right] (s/split st #"\.(?=[^\.]*$)")]
-      (if right
-        [left right]
-        [nil (str left)])))
+(defn split-at-last-dot [st]
+  (let [[left right] (s/split st #"\.(?=[^\.]*$)")]
+    (if right
+      [left right]
+      [nil (str left)])))
 
-  (defn ^String ->vimson [x]
-    (cond
-      (vector? x)
-      (str "[" (s/join ", " (map ->vimson x)) "]")
+(defn ^String ->vimson [x]
+  (cond
+    (vector? x)
+    (str "[" (s/join ", " (map ->vimson x)) "]")
 
-      (keyword? x)
-      (pr-str (name x))
+    (keyword? x)
+    (pr-str (name x))
 
-      (map? x)
-      (str "{"
-           (s/join ", " (map #(str (->vimson (first %)) ":" (->vimson (second %))) x))
-           "}")
+    (map? x)
+    (str "{"
+         (s/join ", " (map #(str (->vimson (first %)) ":" (->vimson (second %))) x))
+         "}")
 
-      :else (pr-str x)))
+    :else (pr-str x)))
 
-  (defn to-hashmap [darr]
-    (reduce (fn [acc [k v]]
+(defn to-hashmap [darr]
+  (reduce (fn [acc [k v]]
             (assoc acc k (conj (get acc k []) v)))
           {} darr))
 
-  (defn search [ns-declare phrase]
-    (eval (read-string ns-declare))
-    (let [given-ns *ns*
-          [given-package given-class+] (split-at-last-dot phrase)
-          java-instance-methods
-          (do
-            (ns neoclojure)
-            (->> (for [[k v] (ns-imports given-ns)
-                       method (.getMethods v)
-                       :let [mname (str "." (.getName method))]
-                       :when (.startsWith mname phrase)]
-                   [mname (.getName v)])
-              set))
-          java-static-methods
+(defn search [ns-declare phrase]
+  (eval (read-string ns-declare))
+  (let [given-ns *ns*
+        [given-package given-class+] (split-at-last-dot phrase)
+        java-instance-methods
+        (do
+          (ns neoclojure)
           (->> (for [[k v] (ns-imports given-ns)
                      method (.getMethods v)
-                     :let [mname (str k "/" (.getName method))]
-                     :when (and
-                             (-> method .getModifiers
-                               java.lang.reflect.Modifier/isStatic)
-                             (.startsWith mname phrase))]
-                 [mname ""])
-            set)
-          java-enum-constants
-          (-> (if given-package
-                (for [[k v] (ns-imports given-ns)
-                      :let [v-package (-> v .getPackage .getName)]
-                      enum (.getEnumConstants v)
-                      :let [class+enum (str k "/" enum)]
-                      :when (and
-                              (= v-package given-package)
-                              (.startsWith class+enum given-class+))]
-                  [(str given-package "." class+enum) ""])
-                (for [[k v] (ns-imports given-ns)
-                      :let [v-package (-> v .getPackage .getName)]
-                      enum (.getEnumConstants v)
-                      :let [class+enum (str k "/" enum)]
-                      :when (.startsWith class+enum given-class+)]
-                  [class+enum v-package]))
-            set)
-          java-namespaces
-          (->> (for [[_ v] (ns-imports given-ns)
-                     :let [fqdn-name (.getName v)]
-                     :when (.startsWith fqdn-name phrase)]
-                 [fqdn-name ""])
-            set)]
-      (->
-        {}
-          (assoc :M (to-hashmap java-instance-methods))
-          (assoc :S (to-hashmap java-static-methods))
-          (assoc :P (to-hashmap java-namespaces))
-          (assoc :E (to-hashmap java-enum-constants))
-        ->vimson)))
-  #_(println (search "(ns aaa (:import [java.net URI]))" ".getN"))
+                     :let [mname (str "." (.getName method))]
+                     :when (.startsWith mname phrase)]
+                 [mname (.getName v)])
+            set))
+        java-static-methods
+        (->> (for [[k v] (ns-imports given-ns)
+                   method (.getMethods v)
+                   :let [mname (str k "/" (.getName method))]
+                   :when (and
+                           (-> method .getModifiers
+                             java.lang.reflect.Modifier/isStatic)
+                           (.startsWith mname phrase))]
+               [mname ""])
+          set)
+        java-enum-constants
+        (-> (if given-package
+              (for [[k v] (ns-imports given-ns)
+                    :let [v-package (-> v .getPackage .getName)]
+                    enum (.getEnumConstants v)
+                    :let [class+enum (str k "/" enum)]
+                    :when (and
+                            (= v-package given-package)
+                            (.startsWith class+enum given-class+))]
+                [(str given-package "." class+enum) ""])
+              (for [[k v] (ns-imports given-ns)
+                    :let [v-package (-> v .getPackage .getName)]
+                    enum (.getEnumConstants v)
+                    :let [class+enum (str k "/" enum)]
+                    :when (.startsWith class+enum given-class+)]
+                [class+enum v-package]))
+          set)
+        java-namespaces
+        (->> (for [[_ v] (ns-imports given-ns)
+                   :let [fqdn-name (.getName v)]
+                   :when (.startsWith fqdn-name phrase)]
+               [fqdn-name ""])
+          set)]
+    (->
+      {}
+      (assoc :M (to-hashmap java-instance-methods))
+      (assoc :S (to-hashmap java-static-methods))
+      (assoc :P (to-hashmap java-namespaces))
+      (assoc :E (to-hashmap java-enum-constants))
+      ->vimson)))
+#_(println (search "(ns aaa (:import [java.net URI]))" ".getN"))
