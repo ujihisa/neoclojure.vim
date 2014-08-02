6488a6f12cb80a530d2eee1fad2eb88a1352f39a
ujihisa <ujihisa@gmail.com>
2014-08-02 13:50:07 -0700

    test for split-at-last-dot

--- autoload/neoclojure.clj
+++ autoload/neoclojure.clj
@@ -2,11 +2,17 @@
   (:require [clojure.string :as s]
             [clojure.repl]))
 
-(defn split-at-last-dot [st]
+(defn
+  ^{:test (fn []
+            (assert (= ["hello" "world"] (split-at-last-dot "hello.world")))
+            (assert (= ["hello.this" "world"] (split-at-last-dot "hello.this..world")))
+            (assert (= [nil "world"] (split-at-last-dot "world"))))}
+  split-at-last-dot [st]
   (let [[left right] (s/split st #"\.(?=[^\.]*$)")]
     (if right
       [left right]
       [nil (str left)])))
+#_ (prn (test #'split-at-last-dot))
 
 (defn ^String ->vimson [x]
   (cond
