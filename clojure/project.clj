b525afcc260ea09d52c704d3f2503dce1e2ed37b
ujihisa <ujihisa@gmail.com>
2015-02-05 17:31:34 -0800

    deps updated

--- clojure/project.clj
+++ clojure/project.clj
@@ -4,12 +4,12 @@
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.5"]
+                 [org.clojure/tools.reader "0.8.13"]
                  [com.cemerick/pomegranate "0.3.0"]
                  [org.clojure/core.incubator "0.1.3"]
                  ; Dirty hack -- without this leininge takes 0.1.2
                  ; which doesn't work with clj-http
-                 [org.apache.httpcomponents/httpclient "4.3.3"]]
+                 [org.apache.httpcomponents/httpclient "4.4"]]
   :target-path "target/%s"
-  :min-lein-version "2.2.0"
+  :min-lein-version "2.5.0"
   :jvm-opts ["-Djava.security.policy=example.policy"]) ; for clojail
