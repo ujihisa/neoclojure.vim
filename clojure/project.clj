04634cc3d4c3dfe380f2aaac908a3dd57ab2d16f
ujihisa <ujihisa@gmail.com>
2015-12-18 01:11:55 -0800

    Use latest clojure itself and tools.reader

--- clojure/project.clj
+++ clojure/project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/ujihisa/neoclojure.vim"
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.13"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+                 [org.clojure/tools.reader "0.10.0"]
                  [com.cemerick/pomegranate "0.3.0"]
                  [org.clojure/core.incubator "0.1.3"]
                  ; Dirty hack -- without this leininge takes 0.1.2
