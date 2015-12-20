53302e62cc841702c652b8a27363a2ab2735d1ea
ujihisa <ujihisa@gmail.com>
2015-12-19 21:05:27 -0800

    Update httpclient

--- clojure/project.clj
+++ clojure/project.clj
@@ -9,7 +9,7 @@
                  [org.clojure/core.incubator "0.1.3"]
                  ; Dirty hack -- without this leininge takes 0.1.2
                  ; which doesn't work with clj-http
-                 [org.apache.httpcomponents/httpclient "4.4"]]
+                 [org.apache.httpcomponents/httpclient "4.5.1"]]
   :target-path "target/%s"
   :min-lein-version "2.5.0"
   :jvm-opts ["-Djava.security.policy=example.policy"]) ; for clojail
