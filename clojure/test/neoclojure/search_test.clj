7304cc1aff28b847d0bb14068380762504b27f86
ujihisa <ujihisa@gmail.com>
2015-12-19 23:56:25 -0800

    Test for ->vimson

--- clojure/test/neoclojure/search_test.clj
+++ clojure/test/neoclojure/search_test.clj
@@ -2,6 +2,14 @@
   (:require [expectations :refer :all]
             [neoclojure.search :refer :all]))
 
+; split-at-last-dot
 (expect ["hello" "world"] (split-at-last-dot "hello.world"))
 (expect ["hello.this" "world"] (split-at-last-dot "hello.this.world"))
 (expect [nil "world"] (split-at-last-dot "world"))
+
+; ->vimson
+(expect "[1]" (->vimson [1]))
+(expect "\"hello\"" (->vimson :hello))
+(expect "{1:2}" (->vimson {1 2}))
+(expect "[1, \"hello\", {}, [], {1:2}, {\"a\":\"b\"}]"
+        (->vimson [1 "hello" {} [] {1 2} {"a" "b"}]))
