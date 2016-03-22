2a7c4dbb4844837261ee70dddea252283e14ef12
Tatsuhiro Ujihisa <ujihisa@gmail.com>
2016-03-21 17:15:04 -0700

    Update dead links

--- test/project.clj
+++ test/project.clj
@@ -4,12 +4,11 @@
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :repositories {"org.bukkit"
-                 "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
+                 "http://repo.bukkit.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "0.9.2"]
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/tools.nrepl "0.2.3"]
-                 [org.bukkit/bukkit "1.7.10-R0.1-SNAPSHOT"]]
-  :main ^:skip-aot cloft2.core
+                 [org.bukkit/bukkit "1.7.9-R0.2"]]
   :target-path "target/%s"
   :min-lein-version "2.2.0")
